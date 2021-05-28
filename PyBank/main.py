8cd759599b02ce3c8969325de414342ccfbdb4e4
stim0010 <scott.stimpon@gmail.com>
2021-05-28 15:40:09 -0400

    Friday Progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,34 +1,48 @@
+
 import csv
 
 ##Define the data path
-budget_csv ='../Resources/Budget_Date.csv'
+budget_csv=('Resources/budget_data.csv')
 
 ##Create function to calculate and display a summary of the Budget
-def printbudsummary(budget_data):
+def budsummary(budget):
 
     ##Data types
-    date = str(budget_data[0])
-    pl = int(budget_data[1])
+    
+    ###what data type is the date & do I even need these variables?
+    #date = str(budget[0])
+    #pl = int(budget[1])
 
     ##Total number of months (count rows)
-    for 
+    total_months = len(list(budget[0]))
 
     ##Net total of profit/losses for the entire period(sum of profit/loss)
-    ntotal=sum(pl)
+    ##need to determine the proper way to sum +/- integers
+    #nptotal=sum(budget[1])
     
     ##Calculate the change from start to end of profit/loss(profit[end]-profit[0])
-    plchange=0
+    ##Use total_months to determine the final row in the data set
+    #plchange=(budget_date[months]-budgetdata[0])
 
     ##Greated increase (max profit/loss)
-    maxpl=max(pl)
+    #maxpl=max(budget[1])
 
     ##Greatest decrease (min profit/loss)
-    minpl=min(pl)
+    #minpl=min(budget[1])
 
-
+    print(f'Number of months = {total_months}')
+    #print(f'{nptotal}')
+    #print(f'{plchange}')
+    #print(f'{maxpl}')
+    #print(f'{minpl}')
+    
+    
 ##Open budget data file
-with open(budget_csv) as csvfile:
+with open(budget_csv, 'r') as csvfile:
 
     csvreader=csv.reader(csvfile, delimiter=",")
 
-    header = next(csvreader)
+    #header = next(csvreader)
+    budsummary()
+
+ 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,24 +5,37 @@ poll_csv = "../Resources/election_data.csv"
 def vote_anal(poll_data):
 
     voter_ID=int(poll_data[0])
-    country=str(poll_data[1])
+    county=str(poll_data[1])
     candidate=str(poll_data[2])
 
 
 
     ##Total votes cast (count row)
+    total_votes=len(list(poll_data[0]))
 
-    ##Complete list of candidates who recieved votes 
-
+    ##Complete list of candidates who recieved votes (list name and remove duplicates)
+    
     ##The percentage of votes each candidate won (candidate votes/total votes)
-
+   ## p_won=(# of candidate)/total_votes
     ##The total number of votes each canidate won (count candidate votes)
 
     ##The winner of the election based on popular vote 
 
+    #print(f'{total_votes}')
+    #print(f'{candidate_list}')
+    #print(f'{})
 
+####FORMAT
+####Election Results
+####----------------
+####Total Votes: {total_votes}
+####----------------
+####candidate(x): {p_won} '('{can_votes}')'
+####----------------
+####Winner: max({can_votes})
 
 with open(poll_csv) as csvfile:
+    
     csvreader=csv.reader(csvfile, delimiter=',')
 
     header=next(csvreader)
