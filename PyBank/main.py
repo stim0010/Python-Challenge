6e18b3c17dae130e0c71093deb362379144ff90d
stim0010 <scott.stimpon@gmail.com>
2021-05-29 15:50:12 -0400

    prior to adding 'while' statement

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,75 +4,38 @@ import csv
 ##Define the data path
 budget_csv=('Resources/budget_data.csv')
 
-##Create function to calculate and display a summary of the Budget
-
-
-
-#def budsummary(budget):
-
-    ##Data types
-    ###what data type is the date & do I even need these variables?
-    
-    #date = str(budget[0])
-    #pl = int(budget[1])
-
-    ##Total number of months (count rows)use counters
-
-    #total_months = 
-
-    ##Net total of profit/losses for the entire period(sum of profit/loss)
-
-    #nptotal =0
-    #nptotal=nptotal + budget[1]
-    
-    ##Calculate the change from start to end of profit/loss(profit[end]-profit[0])
-    ##Use total_months to determine the final row in the data set
-    #plchange=(budget_date[months]- first budgetdata[1])
-
-    ##Greated increase (max profit/loss)
-    #maxpl=max(pl)
-
-    ##Greatest decrease (min profit/loss)
-    #minpl=min(pl)
-    
-    ####################################################################
-    
-    
-   # 
-    #print(f'Average Change: {plchange}')
-    #print(f'Greatest increase in profits: {maxpl}')
-    #print(f'Greatest decrease in profits:{minpl}')
-    
-####Financial Analysis
-####--------------------------
-####Total Months: '{total_months}'
-####Total: '{nptotal}'
-####Average Change: '{plchange}'
-####Greatest increase in profits: {date[0]} + '('{maxpl}')
-####Greatest decrease in profits: {date[0]} + '('{minpl}')
-
-#############################################################################
-
 ##Open budget data file
 with open(budget_csv, 'r') as csvfile:
+    
+    ##Read the csv file
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-    csvreader=csv.reader(csvfile, delimiter=",")
-
+    ##Skip the headers
     header = next(csvreader)
-
+    
+    ##Set count values
     total_months = 0
     nptotal = 0
-    avg_change = 0
+    avgchange = 0
 
     for row in csvreader:
-        total_months = total_months+1
+        
+        ##Find number of months by counting each row
+        total_months = total_months + 1
+        
+        ##Find the total profit/loss by adding each row
         nptotal = nptotal + int(row[1])
         
+        ##Find the average change in profit/loss between each month
+        ##Need to use 'while' to subtract current row from previous row 
+        #avgchange = 
         
-        
+      
     
     print(f'Financial Analysis')
-    print(f'-----------------------------')
+    print(f'---------------------')
     print(f'Total Months: {total_months}')
     print(f'Total: $ {nptotal}')
-    
+    #print(f'Average Change: {avgchange}')
+    #print(f'Greatest increase in profits: {maxpl}')
+    #print(f'Greatest decrease in profits:{minpl}')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,50 +2,6 @@ import csv
 
 poll_csv = "Resources/election_data.csv"
 
-##CREATE FUNCTION 
-
-def vote_analysis(poll_data):
-
-    voter_ID=int(poll_data[0])
-    county=str(poll_data[1])
-    candidate=str(poll_data[2])
-
-    ##Total votes cast (count row)
-    total_votes=0
-    total _votes=total_votes+1
-    
-    ##Complete list of candidates who recieved votes (list names and remove duplicates)
-    
-    
-    ##The percentage of votes each candidate won (candidate votes/total votes)
-    if 
-    ## p_won=(# of votes for candidate)/total_votes
-    
-    ##The total number of votes each canidate won (count candidate votes)
-
-    ##The winner of the election based on popular vote 
-    
-    ###########################################################################
-    
-    #print('Election Results')
-    #print('---------------------')
-    #print(f'Total Votes: {total_votes}')
-    #print('---------------------')
-    #print(f'{candidate_list}')
-    #print(f'{})
-
-####FORMAT
-####Election Results
-####----------------
-####Total Votes: {total_votes}
-####----------------
-####candidate(x): {p_won} + '(' + {can_votes} + ')'
-####----------------
-####Winner: max({can_votes})
-
-
-###############################################################################
-
 
 with open(poll_csv) as csvfile:
     
@@ -53,6 +9,27 @@ with open(poll_csv) as csvfile:
 
     header=next(csvreader)
     
+    total_votes = 0
+    
     for row in csvreader:
-        vote_analysis(row)
+        
+        ##Calc total number of votes by counting the rows
+        total_votes = total_votes + 1
+        
+        ##Use 'while' to determine the candidates and store them in a list
+        ##if currentrow != previous row:
+            ##store
+        
+    
+    print('Election Results')
+    print('---------------------')
+    print(f'Total Votes: {total_votes}')
+    print('---------------------')
+    ##For loop to display the candidate stored in list
+    #print(f'{candidate[]}: {vote_pct} ({can_votes})')
+    print(f'---------------------')
+    #find max(can_votes)
+    #print(f'Winner: {winner}')
+    print(f'---------------------')
+    #print(f'{})
     
